40af646fef5f0f7b41835e4370264bbc52cc00c0
Rolisidamonidze <rolandsidamonidze@gmail.com>
2020-10-23 10:03:38 +0400

    add c# horse race game console app

---
+++ Threading_HorseRaceGame/Horse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Threading_HorseRaceGame {
+	class Horse {
+		public Horse(int row) {
+			Position = 2;
+			Behind = "";
+			Row = row;
+			Win = false;
+			Thread = new Thread(Run);
+		}
+
+		public Thread Thread { get; set; }
+		public int Row { get; set; }
+		public int Position { get; set; }
+		public bool Win { get; set; }
+		public string Behind { get; set; }
+
+		public void Run() {
+			while (!Win) {
+				Thread.Sleep(new Random().Next(20, 120));
+				Position++;
+				Behind += " ";
+			}
+		}
+	}
+}
---
+++ Threading_HorseRaceGame/Race.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Threading_HorseRaceGame {
+	class Race {
+		private List<Horse> _horses;
+		private int _distance;
+
+		public Race(int distance) {
+			Console.CursorVisible = false;
+			_distance = distance;
+			_horses = new List<Horse>();
+		}
+
+		public void AddHorse(Horse horse) {
+			_horses.Add(horse);
+		}
+
+		public void DisplayTrack() {
+			for (int i = 0; i < _horses.Count; i++) {
+				Console.SetCursorPosition(0, i);
+				Console.Write(i);
+				Console.SetCursorPosition(_distance, i);
+				Console.Write("|");
+			}
+		}
+
+		public void Start() {
+			foreach (var horse in _horses) {
+				horse.Thread.Start();
+			}
+			while (WinCheck()) {
+				Display();
+			}
+		}
+
+		private void Display() {
+			DisplayTrack();
+			foreach (var horse in _horses) {
+				Console.SetCursorPosition(2, horse.Row);
+				Console.Write(horse.Behind);
+				Console.SetCursorPosition(horse.Position, horse.Row);
+				Console.WriteLine("-");
+			}
+		}
+
+		private bool WinCheck() {
+			foreach (var horse in _horses) {
+				if (horse.Position >= _distance) {
+					horse.Win = true;
+					Console.SetCursorPosition(_distance / 2, horse.Row + 1);
+					Console.WriteLine($"Horse {horse.Row} Wins!");
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
